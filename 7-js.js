881427131155953b16e44293b8ce5e13a8b2ccde
Tier5 LLC <jon@thinkvaughn.com>
2017-09-06 21:08:49 +0530

    component 2

---
+++ 7-js.js
@@ -0,0 +1,27 @@
+ 
+Vue.component('modal',{
+	template:`
+	<div class="modal is-active">
+
+	  <div class="modal-background"></div>
+	  <div class="modal-content">
+	  <div class="box">
+	   <slot></slot>
+	  </div>
+	  </div>
+	  <button class="modal-close is-large" @click="$emit('close')" aria-label="close"></button>
+	</div>
+	
+	`,
+
+});
+
+
+
+var app = new Vue({
+	el: "#root",
+	data:{
+		showModel:false,
+	}
+	
+});
